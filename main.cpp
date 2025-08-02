95004e1cf9221abe88224d4d5152c5ba5eeb5dc5
bkuolt <mail@bkuolt.de>
2025-08-02 20:54:04 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <glm/glm.hpp>
-#include<nlohmann/json.hpp>
+
 #define TINYGLTF_IMPLEMENTATION
 #define TINYGLTF_NO_STB_IMAGE_WRITE
 #define TINYGLTF_NO_STB_IMAGE
 #define TINYGLTF_NO_INCLUDE_JSON
 #define TINYGLTF_USE_CPP14
 
+#include<nlohmann/json.hpp>
 #include <tiny_gltf.h>
 
 
 #include <iostream>
+
+// Stub für ImageLoader Callback
+bool CustomImageLoader(tinygltf::Image* image, const int image_idx,
+                       std::string* err, std::string* warn,
+                       int req_width, int req_height,
+                       const unsigned char* bytes, int size,
+                       void* user_data) {
+    // Hier könnte Bilddaten verarbeitet werden
+    // Für den Stub einfach true zurückgeben
+    std::cout << "skipping: " << bytes << std::endl;
+    return true;
+}
 
 int main() {
     // Beispiel: GLM Vektor
@@ -20,13 +33,38 @@ int main() {
     tinygltf::TinyGLTF loader;
     tinygltf::Model model;
     std::string err, warn;
-    bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, "model.gltf");
+
+const auto path = "/home/bastian/Desktop/glTF-Sample-Models/2.0/Sponza/glTF/Sponza.gltf";
+
+
+
+/////////////
+
+loader.SetImageLoader(CustomImageLoader, nullptr); // Set custom image loader
+
+/////////////
+
+
+
+    bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, path);
     if (!warn.empty()) std::cout << "Warn: " << warn << std::endl;
     if (!err.empty()) std::cout << "Err: " << err << std::endl;
     if (!ret) {
         std::cout << "Failed to load glTF file" << std::endl;
         return 1;
     }
+
     std::cout << "Loaded glTF file successfully!" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
