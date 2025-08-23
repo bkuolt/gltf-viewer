3bb154e10b3d60dc198c4474b3340539defc923c
bkuolt <mail@bkuolt.de>
2025-08-23 16:19:12 +0200

    refactor buffer

---
+++ src/loader/vk/Buffer.cpp
@@ -0,0 +1,59 @@
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan.hpp>
+#include <array>
+#include <cstring> // für memcpy
+
+#include "Buffer.hpp"
+
+
+Buffer::Buffer(const vk::Device& device,
+        const vk::PhysicalDevice& physicalDevice,
+        vk::BufferUsageFlags usage,
+        vk::DeviceSize bufferSize)
+    : _device(device)
+{
+    vk::BufferCreateInfo bufferInfo{};
+    bufferInfo.size = bufferSize;
+    bufferInfo.usage = usage;
+    bufferInfo.sharingMode = vk::SharingMode::eExclusive;
+    _buffer = device.createBuffer(bufferInfo);
+
+    auto memory = getDeviceMemory(device, _buffer);
+    device.bindBufferMemory(_buffer, memory, 0);
+}
+
+Buffer::~Buffer() noexcept {
+    _device.destroyBuffer(_buffer);
+    _device.freeMemory(_memory);
+}
+
+void Buffer::upload(void* data) {
+    void* mappedMemory = _device.mapMemory(_memory, 0, _bufferSize);
+    std::memcpy(mappedMemory, data, (size_t)_bufferSize);
+    _device.unmapMemory(_memory); 
+}
+
+vk::DeviceMemory Buffer::getDeviceMemory(vk::MemoryPropertyFlags memoryFlags = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) {
+    vk::MemoryRequirements memRequirements = _device.getBufferMemoryRequirements(_buffer);
+    uint32_t memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, memoryFlags);
+
+    vk::MemoryAllocateInfo allocInfo{};
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = memoryTypeIndex;
+    return _device.allocateMemory(allocInfo);
+}
+
+uint32_t Buffer::findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties) {
+    vk::PhysicalDeviceMemoryProperties memProperties = physicalDevice.getMemoryProperties();
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if ((typeFilter & (1 << i)) &&
+            (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            return i;
+        }
+    }
+    throw std::runtime_error("Failed to find suitable memory type!");
+}
+
+
+// = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
--- src/loader/vk/Buffer.hpp
+++ src/loader/vk/Buffer.hpp
@@ -1,88 +1,39 @@
+#ifndef BGL_VK_BUFFER_HPP
+#define BGL_VK_BUFFER_HPP
 #include <vulkan/vulkan.hpp>
 
-// TODO
+class Buffer {
+    public:
+        Buffer(const vk::Device& device,
+               const vk::PhysicalDevice& physicalDevice,
+               vk::BufferUsageFlags usage,
+               vk::DeviceSize bufferSize);
+        virtual ~Buffer() noexcept;
+        
+        void upload(void* data);
 
-VkBuffer createIndexBuffer() {
-    return {};  // TODO
-}
+    private:
+        vk::DeviceMemory getDeviceMemory(vk::MemoryPropertyFlags memoryFlags);
+        uint32_t findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties);
 
-VkBuffer createVertexBuffer() {
-    return {};  // TODO
-}
-
-
-#include <vulkan/vulkan.hpp>
-#include <array>
-#include <cstring> // für memcpy
-
-struct Vertex {
-    float pos[3];
-    float color[3];
+        vk::DeviceSize _bufferSize;
+        vk::Buffer _buffer;
+        vk::DeviceMemory _memory;
+        const vk::Device& _device;
 };
 
-// Beispiel-Vertex-Daten
-std::array<Vertex, 3> vertices = {{
-    {{0.0f, -0.5f, 0.0f}, {1.f, 0.f, 0.f}},
-    {{0.5f, 0.5f, 0.0f},  {0.f, 1.f, 0.f}},
-    {{-0.5f, 0.5f, 0.0f}, {0.f, 0.f, 1.f}},
-}};
+class VertexBuffer : public Buffer {
+    public:
+        VertexBuffer(vk::Device device, vk::PhysicalDevice physicalDevice, vk::DeviceSize size)
+            : Buffer(device, physicalDevice, vk::BufferUsageFlagBits::eVertexBuffer, size)
+        {}
+};
 
-// -----------------------------------------------------
+class IndexBuffer : public Buffer {
+    public:
+        IndexBuffer(vk::Device device, vk::PhysicalDevice physicalDevice, vk::DeviceSize size)
+            : Buffer(device, physicalDevice, vk::BufferUsageFlagBits::eIndexBuffer, size)
+        {}
+};
 
-uint32_t findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties) {
-    vk::PhysicalDeviceMemoryProperties memProperties = physicalDevice.getMemoryProperties();
-
-    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
-        if ((typeFilter & (1 << i)) &&
-            (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
-            return i;
-        }
-    }
-
-    throw std::runtime_error("Failed to find suitable memory type!");
-}
-
-vk::DeviceMemory getDeviceMemory(vk::Device device,
-                                 vk::Buffer buffer, 
-                                 vk::MemoryPropertyFlags memoryFlags = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) {
-
-    // 2. Speicherbedarf abfragen
-    vk::MemoryRequirements memRequirements = device.getBufferMemoryRequirements(buffer);
-    uint32_t memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, memoryFlags);
-
-    // 4. Speicher allokieren
-    vk::MemoryAllocateInfo allocInfo{};
-    allocInfo.allocationSize = memRequirements.size;
-    allocInfo.memoryTypeIndex = memoryTypeIndex;
-    return device.allocateMemory(allocInfo);
-}    
-
-int createBufferExample(vk::BufferUsageFlags usage = vk::BufferUsageFlagBits::eVertexBuffer) {
-    vk::Device device /* dein Vulkan Device */;
-    vk::PhysicalDevice physicalDevice /* dein Physical Device */;
-    vk::DeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
-
-    // Buffer erstellen
-    vk::BufferCreateInfo bufferInfo{};
-    bufferInfo.size = bufferSize;
-    bufferInfo.usage = usage;
-    bufferInfo.sharingMode = vk::SharingMode::eExclusive;
-    vk::Buffer vertexBuffer = device.createBuffer(bufferInfo);
-
-    // Buffer und Speicher verbinden
-    auto memory = getDeviceMemory(device, vertexBuffer);
-    device.bindBufferMemory(vertexBuffer, memory, 0);
-
-    // Daten in Buffer kopieren (map, memcpy, unmap)
-    void* data = device.mapMemory(memory, 0, bufferSize);
-    std::memcpy(data, vertices.data(), (size_t)bufferSize);
-    device.unmapMemory(memory);
-
-    // --- Clean up (später, wenn Buffer nicht mehr gebraucht wird) ---
-    // device.destroyBuffer(vertexBuffer);
-    // device.freeMemory(vertexBufferMemory);
-}
-
-
-// index
-// vertex
+#endif // BGL_VK_BUFFER_HPP
